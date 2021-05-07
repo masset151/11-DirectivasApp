a34fc9f36f9f83b3f75fe44ce490ed4f6a3ed5c9
masset151 <masset.ruiz151@gmail.com@gmail.com>
2021-05-07 11:00:52 +0200

    first commit

---
+++ src/app/productos/pages/agregar/agregar.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-agregar',
+  templateUrl: './agregar.component.html',
+  styleUrls: ['./agregar.component.css']
+})
+export class AgregarComponent {
+
+  miFormulario:FormGroup = this.fb.group({
+    nombre:['',Validators.required]
+  });
+  constructor(private fb:FormBuilder) { }
+
+  tieneError(campo:string):boolean{
+    return this.miFormulario.get(campo)?.invalid || false
+  }
+
+}
