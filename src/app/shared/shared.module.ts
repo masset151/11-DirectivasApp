ee05b37fe0531de25f51494a1179494121463692
masset151 <masset.ruiz151@gmail.com@gmail.com>
2021-05-07 13:51:30 +0200

    Terminado

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ErrorMSGDirective } from './directives/error-msg.directive';
+import { CustomIfDirective } from './directives/custom-if.directive';
 
 
 
 @NgModule({
   declarations: [
-    ErrorMSGDirective
+    ErrorMSGDirective,
+    CustomIfDirective
   ],
   exports: [
-    ErrorMSGDirective
+    ErrorMSGDirective,
+    CustomIfDirective
   ]
 })
 export class SharedModule { }
