37dcce0797354c3899e4e7d4589bcbdb2f1996ef
masset151 <masset.ruiz151@gmail.com@gmail.com>
2021-05-07 11:49:54 +0200

    Reacionar a los cambios en tiempo real

--- src/app/shared/directives/error-msg.directive.ts
+++ src/app/shared/directives/error-msg.directive.ts
@@ -1,15 +1,32 @@
-import { Directive, OnInit } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[error-msg]'
 })
 export class ErrorMSGDirective implements OnInit {
 
-  constructor() { 
+  htmlElement:ElementRef<HTMLElement>
+  @Input() color:string = 'red';
+  @Input()mensaje:string = 'campo requerido'
+
+  constructor(private el:ElementRef<HTMLElement>) { 
     console.log('constructor directive')
+    console.log(el)
+
+    this.htmlElement = el
   }
   ngOnInit(): void {
-   console.log('OnInit Directive')
+   this.setColor()
+   this.setMensaje()
+  }
+
+  setColor(){
+    this.htmlElement.nativeElement.style.color = this.color;
+    this.htmlElement.nativeElement.classList.add('form-text')
+  }
+
+  setMensaje(){
+    this.htmlElement.nativeElement.innerText = this.mensaje;
   }
 
 }
